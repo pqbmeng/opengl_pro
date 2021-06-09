ecb874b2fef4e49a47a0e9cc0e46a4204681f471
sunmeng <sunmeng@ueg.com.cn>
2021-06-09 13:28:01 +0800

    使用着色器绘制三角形

--- pro1/pro1.cpp
+++ pro1/pro1.cpp
@@ -25,29 +25,25 @@ const char *fragmentShaderSource = "#version 330 core\n"
 #endif
 static auto vertexShaderSource = R"(
 #version 330 core
-layout (location = 0) in vec3 aPos;
-out vec4 vertexColor;
+layout (location = 0) in vec3 position; // 位置变量的属性位置值为 0 
+layout (location = 1) in vec3 color;    // 颜色变量的属性位置值为 1
+
+out vec3 ourColor; // 向片段着色器输出一个颜色
+
 void main()
 {
-    gl_Position = vec4(aPos, 1);
-    if (aPos.x > 0)
-    {
-        vertexColor = vec4(1f, 0.0f, 0.0f, 1.0f);
-    }
-    else
-    {
-        vertexColor = vec4(0, 1, 0, 1.0f);
-    }
+    gl_Position = vec4(position, 1.0);
+    ourColor = color; // 将ourColor设置为我们从顶点数据那里得到的输入颜色
 }
 )";
 static auto fragmentShaderSource = R"(
 #version 330 core
-out vec4 FragColor;
-in vec4 vertexColor;
-uniform vec4 ourColor;
+in vec3 ourColor;
+out vec4 color;
+
 void main()
 {
-    FragColor = ourColor;
+    color = vec4(ourColor, 1.0f);
 }
 )";
 
@@ -82,9 +78,10 @@ void pro1::initializeGL()
 
 
 	float vertices[] = {
-		-0.5f, -0.5f, 0.0f, // left  
-		0.5f, -0.5f, 0.0f, // right 
-		0.0f,  0.5f, 0.0f  // top
+        // 位置              // 颜色
+        0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // 右下
+        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // 左下
+        0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // 顶部
 	};
 
 	unsigned int vbo;
@@ -93,10 +90,15 @@ void pro1::initializeGL()
 	glBindVertexArray(vao);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GL_FLOAT), (GLvoid *)0);
 	glEnableVertexAttribArray(0);
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindVertexArray(0);
+
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GL_FLOAT), (GLvoid *)(3 * sizeof(GLfloat)));
+    glEnableVertexAttribArray(1);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0); // unbind vao
 
 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 }
@@ -119,7 +121,7 @@ void pro1::paintGL()
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 #endif
 
-#if 1 // 设置颜色
+#if 0 // 设置颜色
     auto vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
     std::random_device rd;
     std::uniform_real_distribution<> dist(0, 1);
